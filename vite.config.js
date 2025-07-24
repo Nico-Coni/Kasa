62d706639917d8a654a715971e7f6d89de3b4254
Nicolas Morin <nicolas.morin91@gmail.com>
2025-07-24 12:08:58 +0200

    changement de base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Kasa/', // Set the base path for the application
+  base: './', // Set the base path for the application
 })
